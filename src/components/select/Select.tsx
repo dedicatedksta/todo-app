3c0f4f3eda2e57f009b93707ae2fa824a3e760ff
dedicatedksta <amish_lubimov@mail.ru>
2022-09-29 22:55:07 +0300

    w

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,17 +1,23 @@
 import React, { FC } from "react";
-import styles from "./Select.module.css"
+import styles from "./Select.module.css";
 
-interface SelectProps{
-  sort:string,
-  setSort:React.Dispatch<React.SetStateAction<string>>
+interface SelectProps {
+	sort: string;
+	setSort: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Select:FC<SelectProps> = ({sort,setSort}) => {
-  return  <select className={styles.select_wrapper} value={sort} onChange={s=>setSort(s.target.value)} >
-    <option value="all">All todos</option>
-    <option value="complete">Complete</option>
-    <option value="incomplete">Incomplete</option>
-  </select>;
+const Select: FC<SelectProps> = ({ sort, setSort }) => {
+	return (
+		<select
+			className={styles.select_wrapper}
+			value={sort}
+			onChange={(s) => setSort(s.target.value)}
+		>
+			<option value="all">All todos</option>
+			<option value="complete">Complete</option>
+			<option value="incomplete">Incomplete</option>
+		</select>
+	);
 };
 
 export default Select;
